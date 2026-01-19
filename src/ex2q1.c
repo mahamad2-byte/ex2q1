11416346ba02e3bc7643864bf5ed04e907f040b9
Nosaiba <nosaiba@Nosaibas-MacBook-Pro.local>
2026-01-19 00:02:02 -0700

    Add ex2q1 checkout program

---
+++ src/ex2q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    float lab1, lab2, lab3, lab4, lab5;
+    int penalty =0;
+    float penealties_time = 0.0;
+    scanf("%f %f %f %f %f", &lab1, &lab2, &lab3, &lab4, &lab5);
+    int read_values = (scanf("%d %f", &penalty, $penealties_time) == 2) {
+        peenalities_time = penaltuy * penalties_time;
+    }
+    float total_second = lab1 + l
